4886975f6c63b5850465eff49113c848ca6efb94
Jaga Santagostino <jagasantagostino@gmail.com>
2019-12-02 12:08:49 +0100

    fix import

--- pkg/gomain.go
+++ pkg/gomain.go
@@ -12,7 +12,7 @@ import "fmt"
 func main() {
 	fmt.Println("hello")
 }`
-	fileutil.SafeWriteFile("main.go", []byte(fileContent))
+	fileutil.SafeWriteFileOrExit("main.go", []byte(fileContent))
 }
 
 func CreateMainTestFile() {
